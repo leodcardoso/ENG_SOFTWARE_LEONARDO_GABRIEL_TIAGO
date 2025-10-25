8f953ba905487319d82b3554c7ba315f79187594
leodcardoso <146145077+leodcardoso@users.noreply.github.com>
2025-10-24 23:19:35 -0300

    Update database path to include 'data' directory

--- Parte 2/EngSoftware/scripts/db.js
+++ Parte 2/EngSoftware/scripts/db.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, '..', 'db.json');
+const dbPath = path.resolve(__dirname, '..', 'data', 'db.json');
 
 async function readDB() {
   try {
